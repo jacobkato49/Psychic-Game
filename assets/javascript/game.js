64e52195d9bdacabf4f0af6cb9f1e26379a6a48f
Jacob Kato <jacobkato49@yahoo.com>
2017-05-12 23:19:13 -0700

    first edit

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+// Put all letters within an array
+
+var letters=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+// Variable for wins
+
+var wins = 0;
+
+// Variable for loses
+
+var loses = 0;
+
+// Variable for guesses left, guessed letters, correct letter guess
