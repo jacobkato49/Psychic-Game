ed200d5b7f5154620c8bb39bf0d0019728e562b9
Jacob Kato <jacobkato49@yahoo.com>
2017-05-12 23:27:26 -0700

    adding more variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-// Put all letters within an array
+// Put all letters within an array (user choices)
 
 var letters=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
@@ -11,3 +11,16 @@ var wins = 0;
 var loses = 0;
 
 // Variable for guesses left, guessed letters, correct letter guess
+var guesses = 11;
+
+var guessesLeft=11; //create a for loop to count down
+
+var lettersGuessed = []; //house user guesses in an array
+
+// Create a reset
+
+
+// Need to create an active display
+
+
+// Computer needs to generate random letter guessed
