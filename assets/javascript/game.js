5b614faa8011020fcb316ec9a5e40dccae7becaf
Jacob Kato <jacobkato49@yahoo.com>
2017-05-14 16:29:20 -0700

    updated javascript and html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,69 @@
-// Put all letters within an array (user choices)
-
-var letters=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-
-// Variable for wins
-
+var alphabetLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// Initial Variables (these are global)
 var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
 
-// Variable for loses
+// guessesSoFar will be an array and will hold user guess
+var guessesSoFar = [];
 
-var loses = 0;
+// Checks the userGuess to make sure it is a "letter" aka real guess
+var userGuess = null;
 
-// Variable for guesses left, guessed letters, correct letter guess
-var guesses = 11;
-
-var guessesLeft=11; //create a for loop to count down
-
-var lettersGuessed = []; //house user guesses in an array
-
-// Create a reset
-
-
-// Need to create an active display
+// Generate a random letter for the user to guess
+// Computer will then store this letter
+var letterToBeGuessed = alphabetLetters[Math.floor(Math.random() * alphabetLetters.length)];
 
 
-// Computer needs to generate random letter guessed
+// Create a onkeyup event for button press
+document.onkeyup = function(event) {
+
+	// When user presses a key, it records it and saves to userGuess
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+
+
+	// Add the user's guess to guessesSoFar array but only if it wasn't already previously picked by the user
+	// also make sure that the character user picks is within the alphabet, and not any non-usable character
+	if (guessesSoFar.indexOf(userGuess) < 0 && alphabetLetters.indexOf(userGuess) >= 0) {
+		guessesSoFar[guessesSoFar.length]=userGuess;
+		// if it is a new letter then decrease remaining guesses by 1
+		guessesLeft--;
+	}
+
+
+
+	// if letterToBeGuessed is same as userGuess then record it as a win
+	// reset guessesLeft to 9, and empty the guessesSoFar array
+	// also have the computer make a new random pick
+	if (letterToBeGuessed == userGuess) {
+		wins++;
+		console.log("You won!");
+		guessesLeft = 9;
+		guessesSoFar = [];
+		letterToBeGuessed = alphabetLetters[Math.floor(Math.random() * alphabetLetters.length)];
+		console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " Guesses so far: " + guessesSoFar + " Computer picked: " + letterToBeGuessed);
+	}
+
+
+
+	// if guessesLeft gets to 0 then record it as a loss
+	// and then reset guessesLeft to 9, and empty the guessesSoFar array
+	// also have the computer make a new random pick
+	if (guessesLeft == 0) {
+		losses++;
+		console.log("You lost!");
+		guessesLeft = 9;
+		guessesSoFar = [];
+		letterToBeGuessed = alphabetLetters[Math.floor(Math.random() * alphabetLetters.length)];
+		console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " Guesses so far: " + guessesSoFar + " Computer picked: " + letterToBeGuessed);
+	}
+
+
+
+	// Push the display to the html
+	var html = "<p><h4>Guess the letter</h4></p>" + "<p><h4>Wins: " + wins + "</h4></p>" + "<p><h4>Losses: " + losses + "</h4></p>" + "<p><h4>Guesses Left: " + guessesLeft + "</h4></p>" + "<p><h4>Your guesses so far: " + guessesSoFar + "</h4></p>";
+	// place html into the game ID
+	document.querySelector("#game").innerHTML = html;
+
+}
